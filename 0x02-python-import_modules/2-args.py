88e0e46ebcb48da627c3c41a621d9b94f96f207a
Linetgithub <awinoojwang22@gmail.com>
2023-08-11 10:19:04 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-# print the number of and list of arguments
+
 if __name__ == "__main__":
     import sys
-    
     arg = sys.argv
     size = len(arg) - 1
 
@@ -10,7 +9,7 @@ if __name__ == "__main__":
         print("{} arguments:".format(size))
         for i in range(1, size + 1):
             print("{}: {}".format(i, arg[i]))
-    
+
     elif size == 0:
         print("{} arguments.".format(size))
 
