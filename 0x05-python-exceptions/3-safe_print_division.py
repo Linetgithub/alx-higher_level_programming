7eb4e78fb4cbd698597a9e7f290bda94cdaa30eb
Linetgithub <awinoojwang22@gmail.com>
2023-08-28 06:42:34 -0700

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+
 def safe_print_division(a, b):
     try:
-        div = a/b
+        div = a / b
         exept(TypeError, ZeroDivisionError):
             div = None
     finally:
