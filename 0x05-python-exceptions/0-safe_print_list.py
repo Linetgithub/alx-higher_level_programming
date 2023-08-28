403c2458174e1ea3621bd03ca9d74a9e981be3c7
Linetgithub <awinoojwang22@gmail.com>
2023-08-28 11:07:41 -0700

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
-
 def safe_print_list(my_list=[], x=0):
-    total = 0
+    ret = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            total += 1
+            ret += 1
         except Exception:
             break
         print("")
-        return (total) 
+        return (ret) 
