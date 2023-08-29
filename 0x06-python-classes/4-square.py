1fc778e6223024ea0ef6eb50c6f5964b60f24148
Linetgithub <awinoojwang22@gmail.com>
2023-08-29 12:28:22 -0700

    task 4,5,6

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,19 +3,19 @@
 
 
 class Square:
-    """Represent a Square."""
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new Square.
+        """Initialize a new square.
 
         Args:
-        size (int): The size of the new Square."""
+        size (int): The size of the new square."""
 
         self.size = size
 
         @property
         def size(self):
-            """Get/set the current size of the Square."""
+            """Get/set the current size of the square."""
             return (self.__size)
 
         @size.setter
@@ -27,5 +27,5 @@ class Square:
             self.__size = value
 
             def area(self):
-                """Return the current area of the Square."""
+                """Return the current area of the square."""
                 return (self.__size * self.__size)
