74bcba41f51b193e266c6f4c35536eda1c611d39
Linetgithub <awinoojwang22@gmail.com>
2023-08-29 03:50:59 -0700

    task 0,1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square module."""
+
+class square:
+    """Defines a square."""
+
+    def__init__(self, size):
+        """Constructor.
+        Args:
+        size: length of side of the square.
+        """
+
+        self.__size = size
