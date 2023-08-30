feea9c9a99976a6134b103ab88cb22bb7ebb0868
Linetgithub <awinoojwang22@gmail.com>
2023-08-30 02:17:22 -0700

    task 10

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Define a MagicClass matching exactly a bytecode provided by Holberton."""
+"""Define a MagicClass matching exactly a bytecode provided by Alx."""
 
 import math
 
@@ -9,14 +9,11 @@ class MagicClass:
 
     def __init__(self, radius=0):
         """Initialize a MagicClass.
-
-        Args:
+        Arg:
         radius (float or int): The radius of the new MagicClass.
         """
-
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-
             raise TypeError("radius must be a number")
         self.__radius = radius
 
